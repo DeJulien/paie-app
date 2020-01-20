91f18e4f040eccd1433f2fb09eb979c462326c7e
Julien Dewost <dewostjulien@gmail.com>
2020-01-20 16:32:16 +0100

    tp2 maj2

--- src/main/java/dev/paie/exec/InsererProfil.java
+++ src/main/java/dev/paie/exec/InsererProfil.java
@@ -1,29 +1,49 @@
 package dev.paie.exec;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 
-import dev.paie.entite.Grade;
+
+import dev.paie.entite.Avantage;
+import dev.paie.entite.Cotisation;
 import dev.paie.entite.ProfilRemuneration;
-import dev.paie.repository.GradeRepository;
+import dev.paie.repository.CotisationRepository;
 import dev.paie.repository.ProfilRemunerationRepository;
 
 //@Controller
 public class InsererProfil implements Runnable{
 	
 	private ProfilRemunerationRepository profilRemunerationRepository;
+	private CotisationRepository cotisationRepository;
 
-	public InsererProfil(ProfilRemunerationRepository profilRemunerationRepository) {
+	public InsererProfil(ProfilRemunerationRepository profilRemunerationRepository, CotisationRepository cotisationRepository) {
 		super();
 		this.profilRemunerationRepository = profilRemunerationRepository;
+		this.cotisationRepository = cotisationRepository;
 		
 	}
 
 	@Override
 	public void run() {
+		List<Cotisation> cotisations=cotisationRepository.findAll();
+		List<Cotisation> cotisationsAjout= new ArrayList();
+		
+		for(Cotisation coti : cotisations)
+		{
+			if(coti.getId()==1)
+			{
+				cotisationsAjout.add(coti);
+			}
+			
+		}
+		
+		//List<Avantage> avantages;
 		ProfilRemuneration profilRemuneration = new ProfilRemuneration();
 		profilRemuneration.setId(4);
 		profilRemuneration.setCode("Invite");
-		//profilRemuneration.setCotisations();
+		profilRemuneration.setCotisations(cotisationsAjout);
 		//profilRemuneration.setAvantages();
 		
 		this.profilRemunerationRepository.save(profilRemuneration);
--- src/main/java/dev/paie/exec/InsererRemuneration.java
+++ src/main/java/dev/paie/exec/InsererRemuneration.java
@@ -1,13 +1,54 @@
 package dev.paie.exec;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+
+import dev.paie.entite.Cotisation;
+import dev.paie.entite.Entreprise;
+import dev.paie.entite.Grade;
+import dev.paie.entite.ProfilRemuneration;
+import dev.paie.entite.RemunerationEmploye;
+import dev.paie.repository.EntrepriseRepository;
+import dev.paie.repository.GradeRepository;
+import dev.paie.repository.ProfilRemunerationRepository;
+import dev.paie.repository.RemunerationEmployeRepository;
 
 @Controller
 public class InsererRemuneration implements Runnable{
 
+	
+	private RemunerationEmployeRepository remunerationEmployeRepository;
+	private GradeRepository gradeRepository;
+	private ProfilRemunerationRepository profilRemunerationRepository;
+	private EntrepriseRepository entrepriseRepository;
+	
+	public InsererRemuneration(RemunerationEmployeRepository remunerationEmployeRepository,GradeRepository gradeRepository, ProfilRemunerationRepository profilRemunerationRepository,EntrepriseRepository entrepriseRepository) {
+		super();
+		this.remunerationEmployeRepository = remunerationEmployeRepository;
+		this.gradeRepository = gradeRepository;
+		this.profilRemunerationRepository= profilRemunerationRepository;
+		this.entrepriseRepository = entrepriseRepository;
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		List<Grade> grade= gradeRepository.findAll();
+		List<ProfilRemuneration> profilRemuneration= profilRemunerationRepository.findAll();
+		List<Entreprise> entreprise= entrepriseRepository.findAll();
+		
+		RemunerationEmploye remunerationEmploye = new RemunerationEmploye();
+		remunerationEmploye.setMatricule("test01");
+		/*remunerationEmploye.setEntreprise();
+		remunerationEmploye.setProfilRemuneration();
+		remunerationEmploye.setGrade();*/
+		remunerationEmploye.setId(1);
+		
+		this.remunerationEmployeRepository.save(remunerationEmploye);
+		
+
 		
 	}
 
